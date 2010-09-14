4966564f29f2154212c583bab8cb85a56fe8bef9
matejcik neasi <ja@matejcik.cz>
2010-09-15 00:56:45 +0200

    basis of cmd_read

--- posix_server.c
+++ posix_server.c
@@ -21,6 +21,7 @@
 struct handle {
 	char *name;
 	FILE *file;
+	int open_rw;
 	DIR *dir;
 
 	struct dir_entry entry;
@@ -36,6 +37,7 @@ struct handle * newhandle(char* name)
 	struct handle * h = xmalloc(sizeof(struct handle));
 	h->name = name;
 	h->file = NULL;
+	h->open_rw = 0;
 	h->dir = NULL;
 	return h;
 }
@@ -51,7 +53,8 @@ void delhandle (struct handle * handle)
 int validate_assign_handle (uint16_t handle, struct data_packet *pkt)
 {
 #define SLASH 1
-#define DOTS 2
+#define DOT1 2
+#define DOT2 3
 #define OTHER 0
 	int state = OTHER;
 	char *c = pkt->data;
@@ -63,10 +66,12 @@ int validate_assign_handle (uint16_t handle, struct data_packet *pkt)
 	//if (*c != '/') return STAT_BADPATH;
 	while (pos < pkt->len) {
 		if (!*c) return STAT_BADPATH;
-		if (state == DOTS && *c == '/') return STAT_BADPATH;
+		if (state == DOT1 && *c == '/') return STAT_BADPATH;
+		if (state == DOT2 && *c == '/') return STAT_BADPATH;
 		if (state == SLASH && *c == '/') return STAT_BADPATH;
 		if (*c == '/') state = SLASH;
-		else if ((state == SLASH || state == DOTS) && *c == '.') state = DOTS;
+		else if (state == SLASH && *c == '.') state = DOT1;
+		else if (state == DOT1 && *c == '.') state = DOT2;
 		else state = OTHER;
 
 		++pos; ++c;
@@ -82,10 +87,11 @@ int validate_assign_handle (uint16_t handle, struct data_packet *pkt)
 	return STAT_OK;
 }
 
-#define VALIDATE_HANDLE(sock, cmd) { \
+#define VALIDATE_HANDLE(h, sock, cmd) { \
 	if (cmd->handle >= MAXHANDLES || handles[cmd->handle] == NULL) { \
 		return send_reply_p(sock, cmd->id, STAT_BADHANDLE); \
 	} \
+	h = handles[cmd->handle]; \
 }
 
 
@@ -97,7 +103,7 @@ int fill_entry (char const * directory, int dlen, struct dirent const * dirent, 
 	int len, ret;
 	struct stat st;
 	assert(entry); assert(dirent);
-	/* entry is pre-allocated and cleared, dirent should already be checked for null */
+	/* entry is pre-allocated, dirent should already be checked for null */
 
 	len = strlen(dirent->d_name);
 	if (entry->len < len) {
@@ -124,22 +130,24 @@ int fill_entry (char const * directory, int dlen, struct dirent const * dirent, 
 
 		entry->size = st.st_size;
 
-		if ((st.st_uid == getuid() && st.st_mode & S_IRUSR)
-		 || (st.st_gid == getgid() && st.st_mode & S_IRGRP)
-		 || (st.st_mode && S_IROTH)) entry->perm |= PERM_READ;
-		if ((st.st_uid == getuid() && st.st_mode & S_IWUSR)
-		 || (st.st_gid == getgid() && st.st_mode & S_IWGRP)
-		 || (st.st_mode && S_IWOTH)) entry->perm |= PERM_WRITE;
+		entry->perm = 0;
+		/* XXX more detailed check for access() failure? */
+		if (!access(path, R_OK)) entry->perm |= PERM_READ;
+		if (!access(path, W_OK)) entry->perm |= PERM_WRITE;
 	}
 
 	return SIZEOF_dir_entry - sizeof(char*) + entry->len;
 }
 
+/* to be used with recv_* and send_* only. file-related syscalls must handle their errors */
 #define MAYBE_RET(what) { \
 	int HANDLEret = what; \
 	if (HANDLEret <= 0) return HANDLEret; \
 }
 
+/* EINTR retrying macros for functions that return 0/null or -1 on failure */
+#define RETRY0(a, what) do { a = what; } while (a == 0 && errno == EINTR)
+#define RETRY1(a, what) do { a = what; } while (a == -1 && errno == EINTR)
 
 /**** actual command implementations ****/
 
@@ -155,8 +163,7 @@ int cmd_assign (int sock, struct command *cmd)
 int cmd_list (int sock, struct command *cmd)
 {
 	struct handle * h;
-	VALIDATE_HANDLE(sock, cmd);
-	h = handles[cmd->handle];
+	VALIDATE_HANDLE(h, sock, cmd);
 
 	if (h->dir) closedir(h->dir);
 	h->dir = opendir(h->name);
@@ -183,8 +190,7 @@ int cmd_list_cont (int sock, struct command *cmd)
 	int dlen;
 	struct dirent * dirent;
 	struct handle * h;
-	VALIDATE_HANDLE(sock, cmd);
-	h = handles[cmd->handle];
+	VALIDATE_HANDLE(h, sock, cmd);
 
 	dlen = strlen(h->name);
 
@@ -197,6 +203,10 @@ int cmd_list_cont (int sock, struct command *cmd)
 #define BUFLEN 128000
 	buffer = xmalloc(BUFLEN);
 	while ((dirent = readdir(h->dir))) {
+		/* skip "." and ".." */
+		if (dirent->d_name[0] == '.')
+			if (dirent->d_name[1] == 0 || (dirent->d_name[1] == '.' && dirent->d_name[2] == 0)) continue;
+		/* fill the entry */
 		h->elen = fill_entry(h->name, dlen, dirent, &h->entry);
 		if (h->entry.type == ENTRY_BAD) continue;
 		if (filled + h->elen < BUFLEN) {
@@ -225,8 +235,43 @@ int cmd_list_cont (int sock, struct command *cmd)
 
 int cmd_read (int sock, struct command *cmd)
 {
-	return -1;
+	struct handle * h;
+	int err;
+	long pos;
+	struct params_offlen params;
+
+	/* be a good guy and pick parameters first */
+	MAYBE_RET(recv_params_offlen(sock, &params));
+
+	VALIDATE_HANDLE(h, sock, cmd);
+
+	if (!h->file) {
+		RETRY0(h->file, fopen(h->name, "r"));
+		err = STAT_OK;
+		if (!h->file) { /* open failed */
+			if (errno == EACCES) err = STAT_EACCESS;
+			else if (errno == EISDIR) err = STAT_NOTFILE;
+			else if (errno == ENOENT) err = STAT_NOTFOUND;
+			else if (errno == ENOTDIR) err = STAT_BADPATH;
+			else err = STAT_SERVFAIL;
+			return send_reply_p(sock, cmd->id, err);
+		}
+	}
+	/* TODO check whether the open handle belongs to the correct path */
+	RETRY1(err, fseek(h->file, params->offset, SEEK_SET));
+	if (err == -1) {
+		if (errno == EINVAL) {
+			/* offset beyond end of file - return an empty read */
+			MAYBE_RET(send_reply_p(sock, cmd->id, STAT_OK));
+			return send_data(sock, NULL, 0);
+		} else {
+			/* something bad went wronger */
+			return send_reply_p(sock, cmd->id, STAT_SERVFAIL);
+		}
+	}
+
 }
+
 int cmd_write (int sock, struct command *cmd)
 {
 	return -1;
